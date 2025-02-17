fd84cf347a34a63d42dbe0c6429e5d6ae5d45f83
Dan Schulte <dan.schulte.qub@live.com>
2025-02-16 19:20:52 -0800

    Mostly got ArrayList working, but it has the flaw that its elements must have a default constructor. Need to fix that.

--- sources/ArrayList.h
+++ sources/ArrayList.h
@@ -43,24 +43,58 @@ namespace e1
         }
 
         /**
-         * Add the provided value onto the end of this ArrayList.
-         * @param value The value to add.
+         * Insert the provided value into this ArrayList at the provided index. All values at and
+         * after the provided index will be shifted to the right one index position.
+         * @param index The index to insert the value at.
+         * @param value The value to insert.
          */
-        const P<ArrayList<T>> add(const T& value)
+        const P<ArrayList<T>> insert(int index, const T& value)
         {
             const int currentCount = this->getCount();
             if (!this->array.hasValue() ||
                 this->array->getCount() == currentCount)
             {
                 const P<Array<T>> newArray = this->getAllocator()->template createArray<T>((2 * currentCount) + 1);
-                for (int i = 0; i < currentCount; i++)
+                for (int i = 0; i < index; i++)
                 {
                     newArray->set(i, this->get(i));
                 }
+                for (int i = index; i < currentCount; i++)
+                {
+                    newArray->set(i + 1, this->get(i));
+                }
                 this->array.set(newArray);
             }
-            this->array->set(currentCount, value);
+            else
+            {
+                for (int i = currentCount; index < i; i--)
+                {
+                    this->set(i, this->get(i - 1));
+                }
+            }
+            this->set(index, value);
             this->count++;
+
+            return this->getThisPointer();
+        }
+
+        /**
+         * Add the provided value onto the end of this ArrayList.
+         * @param value The value to add.
+         */
+        const P<ArrayList<T>> add(const T& value)
+        {
+            return this->insert(this->getCount(), value);
+        }
+
+        /**
+         * Set the value at the provided index.
+         * @param index The index to set.
+         * @param value The value to set.
+         */
+        const P<ArrayList<T>> set(int index, const T& value)
+        {
+            this->array->set(index, value);
 
             return this->getThisPointer();
         }
@@ -72,6 +106,25 @@ namespace e1
         T& get(int index) const
         {
             return this->array->get(index);
+        }
+
+        /**
+         * Remove and return the value at the provided index. All values after the provided index
+         * will be shifted to the left one index position.
+         * @param index The index at which to remove a value.
+         */
+        const T removeAt(int index)
+        {
+            const T result = this->get(index);
+
+            const int currentCount = this->getCount();
+            for (int i = index; i < currentCount - 1; i++)
+            {
+                this->set(i, this->get(i + 1));
+            }
+            this->count--;
+
+            return result;
         }
 
         const P<ArrayList<T>> getThisPointer() const
--- sources/ArrayListTests.cpp
+++ sources/ArrayListTests.cpp
@@ -47,6 +47,89 @@ namespace e1
                 });
             });
 
+            runner->testMethod("insert()", [&]
+            {
+                runner->test("when ArrayList is empty", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>(&allocator);
+
+                    const P<ArrayList<int>> result = list->insert(0, 5);
+
+                    test.assertEqual(result, list);
+                    test.assertEqual(1, list->getCount());
+                    test.assertEqual(5, list->get(0));
+                });
+
+                runner->test("when ArrayList has one value and index is 0", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>(&allocator)->add(0);
+
+                    const P<ArrayList<int>> result = list->insert(0, 1);
+
+                    test.assertEqual(result, list);
+                    test.assertEqual(2, list->getCount());
+                    test.assertEqual(1, list->get(0));
+                    test.assertEqual(0, list->get(1));
+                });
+
+                runner->test("when ArrayList has one value and index is 1", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>(&allocator)->add(0);
+
+                    const P<ArrayList<int>> result = list->insert(1, 1);
+
+                    test.assertEqual(result, list);
+                    test.assertEqual(2, list->getCount());
+                    test.assertEqual(0, list->get(0));
+                    test.assertEqual(1, list->get(1));
+                });
+
+                runner->test("when ArrayList has two values and index is 0", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>(&allocator)->add(0)->add(1);
+
+                    const P<ArrayList<int>> result = list->insert(0, 2);
+
+                    test.assertEqual(result, list);
+                    test.assertEqual(3, list->getCount());
+                    test.assertEqual(2, list->get(0));
+                    test.assertEqual(0, list->get(1));
+                    test.assertEqual(1, list->get(2));
+                });
+
+                runner->test("when ArrayList has two values and index is 1", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>(&allocator)->add(0)->add(1);
+
+                    const P<ArrayList<int>> result = list->insert(1, 2);
+
+                    test.assertEqual(result, list);
+                    test.assertEqual(3, list->getCount());
+                    test.assertEqual(0, list->get(0));
+                    test.assertEqual(2, list->get(1));
+                    test.assertEqual(1, list->get(2));
+                });
+
+                runner->test("when ArrayList has two values and index is 2", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>(&allocator)->add(0)->add(1);
+
+                    const P<ArrayList<int>> result = list->insert(2, 2);
+
+                    test.assertEqual(result, list);
+                    test.assertEqual(3, list->getCount());
+                    test.assertEqual(0, list->get(0));
+                    test.assertEqual(1, list->get(1));
+                    test.assertEqual(2, list->get(2));
+                });
+            });
+
             runner->testMethod("add()", [&]
             {
                 runner->test("when ArrayList is empty", [](Test test)
@@ -54,9 +137,9 @@ namespace e1
                     HeapAllocator allocator;
                     const P<ArrayList<int>> list = allocator.create<ArrayList<int>>(&allocator);
 
-                    const P<ArrayList<int>> addResult = list->add(5);
+                    const P<ArrayList<int>> result = list->add(5);
 
-                    test.assertEqual(addResult, list);
+                    test.assertEqual(result, list);
                     test.assertEqual(1, list->getCount());
                     test.assertEqual(5, list->get(0));
                 });
@@ -66,9 +149,116 @@ namespace e1
                     HeapAllocator allocator;
                     const P<ArrayList<int>> list = allocator.create<ArrayList<int>>(&allocator)->add(0);
 
-                    const P<ArrayList<int>> addResult = list->add(1);
+                    const P<ArrayList<int>> result = list->add(1);
 
-                    test.assertEqual(addResult, list);
+                    test.assertEqual(result, list);
+                    test.assertEqual(2, list->getCount());
+                    test.assertEqual(0, list->get(0));
+                    test.assertEqual(1, list->get(1));
+                });
+
+                runner->test("when ArrayList has more than one value", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>()
+                        ->add(0)
+                        ->add(1);
+
+                    const P<ArrayList<int>> result = list->add(2);
+
+                    test.assertEqual(result, list);
+                    test.assertEqual(3, list->getCount());
+                    test.assertEqual(0, list->get(0));
+                    test.assertEqual(1, list->get(1));
+                    test.assertEqual(2, list->get(2));
+                });
+            });
+
+            runner->testMethod("removeAt()", [&]
+            {
+                runner->test("with one value and index 0", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>()
+                        ->add(1);
+
+                    const int result = list->removeAt(0);
+
+                    test.assertEqual(1, result);
+                    test.assertEqual(0, list->getCount());
+                });
+
+                runner->test("with two values and index 0", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>()
+                        ->add(0)
+                        ->add(1);
+
+                    const int result = list->removeAt(0);
+
+                    test.assertEqual(0, result);
+                    test.assertEqual(1, list->getCount());
+                    test.assertEqual(1, list->get(0));
+                });
+
+                runner->test("with two values and index 1", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>()
+                        ->add(0)
+                        ->add(1);
+
+                    const int result = list->removeAt(1);
+
+                    test.assertEqual(1, result);
+                    test.assertEqual(1, list->getCount());
+                    test.assertEqual(0, list->get(0));
+                });
+
+                runner->test("with three values and index 0", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>()
+                        ->add(0)
+                        ->add(1)
+                        ->add(2);
+
+                    const int result = list->removeAt(0);
+
+                    test.assertEqual(0, result);
+                    test.assertEqual(2, list->getCount());
+                    test.assertEqual(1, list->get(0));
+                    test.assertEqual(2, list->get(1));
+                });
+
+                runner->test("with three values and index 1", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>()
+                        ->add(0)
+                        ->add(1)
+                        ->add(2);
+
+                    const int result = list->removeAt(1);
+
+                    test.assertEqual(1, result);
+                    test.assertEqual(2, list->getCount());
+                    test.assertEqual(0, list->get(0));
+                    test.assertEqual(2, list->get(1));
+                });
+
+                runner->test("with three values and index 2", [](Test test)
+                {
+                    HeapAllocator allocator;
+                    const P<ArrayList<int>> list = allocator.create<ArrayList<int>>()
+                        ->add(0)
+                        ->add(1)
+                        ->add(2);
+
+                    const int result = list->removeAt(2);
+
+                    test.assertEqual(2, result);
                     test.assertEqual(2, list->getCount());
                     test.assertEqual(0, list->get(0));
                     test.assertEqual(1, list->get(1));
